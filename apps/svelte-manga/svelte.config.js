c8816e97f7720f9a4cb50a13b8de967a62254853
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2023-10-17 22:24:57 +0200

    Gha init (#25)
    * gg
    
    * GHCR_PAT
    
    * name: Login to GitHub Container Registry
            if: github.event_name != 'pull_request'
            uses: docker/login-action@v3
            with:
              registry: ghcr.io
              username: ${{ github.actor }}
              password: ${{ secrets.GITHUB_TOKEN }}
    
    * secrets.GITHUB_TOKEN not logging in in gihub action.
    Error: Username and password required
    
    * permissions:
          packages: write
          contents: read
    
    * if: github.event_name != 'pull_request'
    
    * manifest
    
    * remove manifest
    
    * deploy
    
    * login
    
    * |
            flyctl auth login -t ${{ secrets.FLY_API_TOKEN }}
    
    * h
    
    * fly
    
    * fly
    
    * ./apps/svelte-manga
    
    * docker-compose -f docker-compose.yml up svelte-manga
    
    * COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f docker-compose.yml build svelte-manga
    
    * ./apps/svelte-manga/Dockerfile
    
    * flyctl deploy --remote-only --image valiantlynx/svelte-manga:latest
    
    * dd
    
    * if: github.event_name != 'pull_request'
    
    * uu
    
    * ${{ secrets.DOCKER_HUB_USERNAME }}/svelte-manga
    
    * ${{ secrets.DOCKER_HUB_USERNAME }}/svelte-manga
    
    * flyctl deploy --remote-only --image your-image-name:tag --app svelte-manga
    
    * no fly
    
    * flyctl deploy --remote-only --image your-image-name:tag --app svelte-manga
    
    ---------
    
    Co-authored-by: Gormery Kombo Wanjiru <gormerykw@uia.no>

--- apps/svelte-manga/svelte.config.js
+++ apps/svelte-manga/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,7 +6,6 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
