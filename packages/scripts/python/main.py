e0eb467031f8ec3a563a1002668583cb47298efa
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-10-18 06:27:07 +0200

    issue_discord_message

--- packages/scripts/python/main.py
+++ packages/scripts/python/main.py
@@ -1,21 +1,67 @@
 import argparse
+from weather.weather import WeatherNotifier
 from message_discord.message_discord import DiscordMessenger
-from weather.weather import main
+import os
+import sys
+import json
+
+def weather_discord_message():
+    if __name__ == '__main__':
+        discord_webhook_url = os.environ['DISCORD_WEBHOOK_URL']
+        discord_messenger = DiscordMessenger(discord_webhook_url)
+
+        weather_notifier = WeatherNotifier(discord_messenger)
+        weather_notifier.main()
 
 
-def function1():
-    # Implement your logic for function 1 here
+def issue_discord_message():
+    discord_webhook_url = os.environ['DISCORD_WEBHOOK_URL']
 
-def function2():
-    # Implement your logic for function 2 here
+    # Create an instance of DiscordMessenger
+    discord_messenger = DiscordMessenger(discord_webhook_url)
+
+    # Parse the JSON input
+    try:
+        issue_data = json.loads(sys.argv[1])
+        issue_type = sys.argv[2]
+    except json.JSONDecodeError:
+        issue_data = {}
+
+    # Extract issue details
+    title = issue_data.get('title', 'No title provided')
+    body = issue_data.get('body', 'No body provided')
+    user = issue_data.get('user', 'Unknown user')
+    avatar_url = issue_data.get('avatar_url', '')
+    html_url = issue_data.get('html_url', '')
+
+    # Use the issue details to create a custom message
+    custom_message = f"Issue - **{title}**\n-Description: - {body}\nUser: **{user}**\n[Click Here]({html_url})"
+
+    message_data = {
+        "title": title,
+        "custom_message": custom_message,
+        "user": user,
+        "avatar_url": avatar_url,
+        "html_url": html_url,
+    }
+
+    # Send a message to Discord
+    if issue_type == 'error':
+        discord_messenger.send_error(message_data)
+    elif issue_type == 'warning':
+        discord_messenger.send_warning(message_data)
+    elif issue_type == 'info':
+        discord_messenger.send_info(message_data)
+    else:
+        discord_messenger.send_message(message_data)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run specific functions')
-    parser.add_argument('function_name', choices=['function1', 'function2'], help='Name of the function to run')
+    parser.add_argument('function_name', choices=['weather_discord_message', 'issue_discord_message'], help='Name of the function to run')
 
     args = parser.parse_args()
 
-    if args.function_name == 'function1':
-        function1()
-    elif args.function_name == 'function2':
-        function2()
+    if args.function_name == 'weather_discord_message':
+        weather_discord_message()
+    elif args.function_name == 'issue_discord_message':
+        issue_discord_message()
--- packages/scripts/python/weather/weather.py
+++ packages/scripts/python/weather/weather.py
@@ -1,60 +1,53 @@
 import requests
-import os
 
-from message_discord.message_discord import DiscordMessenger
+class WeatherNotifier:
+    def __init__(self, discord_messenger):
+        self.discord_messenger = discord_messenger
 
-discord_webhook_url = os.environ['DISCORD_WEBHOOK_URL']
+    # Configuration
+    api_key = 'f7bb0829a3ba41678b921402231810'  # Replace with your OpenWeatherMap API key
+    location = 'grimstad'  # Replace with your desired location
 
-# Create an instance of DiscordMessenger
-discord_messenger = DiscordMessenger(discord_webhook_url)
+    # Fetch weather data
+    def fetch_weather_data(self):
+        api_url = f'https://api.weatherapi.com/v1/current.json?q={self.location}&key={self.api_key}'
+        try:
+            response = requests.get(api_url)
+            if response.status_code != 200:
+                raise Exception('Unable to fetch weather data')
+            data = response.json()
+            return data
+        except Exception as error:
+            print('Error:', error)
+            return None
 
+    # Send a notification
+    def send_notification(self, weather_data):
+        if not weather_data:
+            return
 
-# Configuration
-api_key = 'f7bb0829a3ba41678b921402231810'  # Replace with your OpenWeatherMap API key
-location = 'grimstad'  # Replace with your desired location
+        current = weather_data['current']
+        temperature = current['temp_c']
+        weather_description = current['condition']['text']
+        feels_like = current['feelslike_c']
 
-# Fetch weather data
-def fetch_weather_data():
-    api_url = f'https://api.weatherapi.com/v1/current.json?q={location}&key={api_key}'
-    try:
-        response = requests.get(api_url)
-        if response.status_code != 200:
-            raise Exception('Unable to fetch weather data')
-        data = response.json()
-        return data
-    except Exception as error:
-        print('Error:', error)
-        return None
+        message = f'Temperature: {temperature}°C\nConditions: {weather_description}. but it feels like {feels_like}°C'
 
-# Send a notification
-def send_notification(weather_data):
-    if not weather_data:
-        return
+        message_data = {
+            "title": "Weather in Grimstad",
+            "custom_message": message,
+            "user": "Weather Bot",
+            "avatar_url": current['condition']['icon'],
+            "html_url": "https://www.valiantlynx.com",
+        }
 
-    current = weather_data['current']
-    temperature = current['temp_c']
-    weather_description = current['condition']['text']
-    feels_like = current['feelslike_c']
+        self.discord_messenger.send_message(message_data)
 
-    message = f'Temperature: {temperature}°C\nConditions: {weather_description}. but it feels like {feels_like}°C'
-    
-    message_data = {
-    "title": "Weather in Grimstad",
-    "custom_message": message,
-    "user": "Weather Bot",
-    "avatar_url": current['condition']['icon'],
-    "html_url": "https://www.valiantlynx.com",
-}
+    # Main function
+    def main(self):
+        try:
+            weather_data = self.fetch_weather_data()
+            self.send_notification(weather_data)
+        except Exception as error:
+            print('Error:', error)
 
-    discord_messenger.send_message(message_data)
-
-# Main function
-def main():
-    try:
-        weather_data = fetch_weather_data()
-        send_notification(weather_data)
-    except Exception as error:
-        print('Error:', error)
-
-if __name__ == '__main__':
-    main()
