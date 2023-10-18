65d3032f0343ef73633e9710dae2225747ffe098
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-10-18 07:01:17 +0200

    f

--- packages/scripts/python/logger/logger.py
+++ packages/scripts/python/logger/logger.py
@@ -17,15 +17,15 @@ logger_file_handler.setFormatter(formatter)
 logger.addHandler(logger_file_handler)
 
 try:
-    SOME_SECRET = os.environ["SOME_SECRET"]
+    HOST_URL = os.environ["HOST_URL"]
 except KeyError:
-    SOME_SECRET = "Token not available!"
+    HOST_URL = "Token not available!"
     #logger.info("Token not available!")
     #raise
 
 
 if __name__ == "__main__":
-    logger.info(f"Token value: {SOME_SECRET}")
+    logger.info(f"Host url: {HOST_URL}")
 
     r = requests.get('https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
     if r.status_code == 200:
