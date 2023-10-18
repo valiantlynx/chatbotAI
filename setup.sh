c48763516a5192e34cf190c21422100078d117d2
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-10-18 12:02:19 +0200

    make a brach on the main repo named the same as the monorepo

--- setup.sh
+++ setup.sh
@@ -2,12 +2,13 @@ docker build --no-cache -t chatbotai-main .
 docker run --name chatbotai-main -d -p 8000:8000 -v $(pwd):/code chatbotai-main:latest 
 docker exec chatbotai-main /bin/bash
 
+# make a brach on the main repo named the same as the monorepo
 # add this as a subtree to the main repo
-git subtree add --prefix=apps/chatbotAI https://github.com/valiantlynx/chatbotAI.git main --squash
+git subtree add --prefix=apps/chatbotAI https://github.com/valiantlynx/chatbotAI.git valiantlynx-turborepo --squash
 
 # pull the subtree
-git subtree pull --prefix=apps/chatbotAI https://github.com/valiantlynx/chatbotAI.git main --squash
+git subtree pull --prefix=apps/chatbotAI https://github.com/valiantlynx/chatbotAI.git valiantlynx-turborepo --squash
 
 # push the subtree
-git subtree push --prefix=apps/chatbotAI https://github.com/valiantlynx/chatbotAI.git main
+git subtree push --prefix=apps/chatbotAI https://github.com/valiantlynx/chatbotAI.git valiantlynx-turborepo
 
